2ab40937ea321ddfa96ac8374ccfc762d075bcae
akjn23 <55597629+akjn23@users.noreply.github.com>
2019-10-25 11:06:23 +0530

    Update Codechef.java

--- Codechef.java
+++ Codechef.java
@@ -7,7 +7,7 @@ import java.io.*;
 /* Name of the class has to be "Main" only if the class is public. */
 class Codechef
 {
-    
+    try
 	public static void main (String[] args) throws java.lang.Exception 
 	{
 	    
@@ -38,7 +38,11 @@ class Codechef
 		for(i=0;i<n;i++)
 		    System.out.println(z[i]);
 	    }
-	    
+}
+ 	catch(Exception e)
+	{
+		System.out.println(e);
+	}
 		    
 		    
 		    
